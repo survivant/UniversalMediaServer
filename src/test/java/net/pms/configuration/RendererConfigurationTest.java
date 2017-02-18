5887f59a31175470f243cf39382525d5c688d411
Samuel <Sami32@users.noreply.github.com>
2017-02-19 00:26:29 +0100

    Add PS4 Pro renderer

--- src/test/java/net/pms/configuration/RendererConfigurationTest.java
+++ src/test/java/net/pms/configuration/RendererConfigurationTest.java
@@ -132,18 +132,11 @@ public class RendererConfigurationTest {
 		testHeaders("Showtime 4", "User-Agent: Showtime PS3 4.2");
 
 		// Sony PlayStation 3:
-		testHeaders(
-			"PlayStation 3",
-			"User-Agent: PLAYSTATION 3",
-			"X-AV-Client-Info: av=5.0; cn=\"Sony Computer Entertainment Inc.\"; mn=\"PLAYSTATION 3\"; mv=\"1.0\";"
-		);
+		testHeaders("PlayStation 3", "User-Agent: PLAYSTATION 3", "PlayStation 3", "X-AV-Client-Info: av=5.0; cn=\"Sony Computer Entertainment Inc.\"; mn=\"PLAYSTATION 3\"; mv=\"1.0\";");
 
 		// Sony PlayStation 4:
-		testHeaders(
-			"PlayStation 4",
-			"User-Agent: PS4Application libhttp/1.000 (PS4) libhttp/2.51 (PlayStation 4)",
-			"User-Agent: libhttp/2.51 (PlayStation 4)"
-		);
+		testHeaders("PlayStation 4", "User-Agent: PS4Application libhttp/1.000 (PS4) libhttp/2.51 (PlayStation 4)");
+		testHeaders("PlayStation 4", "User-Agent: libhttp/2.51 (PlayStation 4)");
 
 		// Sony Xperia:
 		testHeaders("Sony Xperia Z/ZL/ZQ/Z1/Z2", "X-AV-Client-Info: C6603");
